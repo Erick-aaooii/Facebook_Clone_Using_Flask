89156068f7e63b14bc73f509ec1da14db8f3f989
Erick-aaooii <andreserick2004@gmail.com>
2024-06-30 13:37:33 -0300

    version 1.7

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from flask import Flask, render_template, url_for, request
 app = Flask(__name__, template_folder="html_folder", static_folder="styles")
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hompage():
+    email = request.form.get('email')
+    password = request.form.get('password')
     return render_template("index.html")
+
 
 
 if __name__ == '__main__':
